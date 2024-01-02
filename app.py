2380195e257f960bb1f1b3cca355c72553a20bed
Emanuele Mancari <emanuele.mancari@abstract.it>
2024-01-02 15:43:39 +0100

    try to fix logs in production

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from flask_socketio import SocketIO
 from authlib.integrations.flask_client import OAuth
 from dotenv import load_dotenv
 import os
+import logging
 
 load_dotenv()
 
@@ -52,22 +53,22 @@ def load_user(user_id):
     return User.get_or_none(User.id == int(user_id))
 
 def iss_tracker():
-    print('iss_tracker def')
+    app.logger.debug('iss_tracker def')
     while True:
-        print('iss_tracker while')
+        app.logger.debug('iss_tracker while')
         position = get_iss_position()
         socketio.emit('update_position', {'latitude': position['latitude'], 'longitude': position['longitude']})
-        print(position['latitude'], position['longitude'])
+        app.logger.debug(position['latitude'], position['longitude'])
         time.sleep(10)  
 
 def get_iss_position():
-    print('get_iss_position def')
+    app.logger.debug('get_iss_position def')
     url = "https://api.wheretheiss.at/v1/satellites/25544"
     payload={}
     headers = {}
     response = requests.request("GET", url, headers=headers, data=payload)
     location = json.loads(response.text)
-    print('get_iss_position location: ', location)
+    app.logger.debug('get_iss_position location: ', location)
     return(location)
 
 def find_or_create_google_user(userinfo):
@@ -193,10 +194,13 @@ def new_user():
     return render_template('new_user.html')
 
 if __name__ == '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
     db.connect()
     db.create_tables([User], safe=True)
     threading.Thread(target=iss_tracker, daemon=True).start()
     socketio.run(app)
-    print('end py code')
+    app.logger.debug('end py code')
     #socketio.run(app, debug=True, port=5001)
     #app.run(debug=True, port=8080)
